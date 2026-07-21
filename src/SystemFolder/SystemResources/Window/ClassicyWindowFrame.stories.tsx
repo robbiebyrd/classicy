775f5612c0ce159cd7f7e6f86752dd2bdc4013dd
Robbie Byrd <robbie.byrd@level.co>
2026-07-21 04:01:32 +0000

    fix(window): use @storybook/react-vite in ClassicyWindowFrame story

--- src/SystemFolder/SystemResources/Window/ClassicyWindowFrame.stories.tsx
+++ src/SystemFolder/SystemResources/Window/ClassicyWindowFrame.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { ClassicyButton } from "@/SystemFolder/SystemResources/Button/ClassicyButton";
 import { ClassicyWindowFrame } from "@/SystemFolder/SystemResources/Window/ClassicyWindowFrame";
 
